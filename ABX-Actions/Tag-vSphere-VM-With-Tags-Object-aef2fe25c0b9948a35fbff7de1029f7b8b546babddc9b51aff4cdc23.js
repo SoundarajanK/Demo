5c9f5764bb198f8c14e0b62a0e8ce4094e3a8bff
pawwann112 <59345192+pawwann112@users.noreply.github.com>
2023-03-07 01:05:36 +0530

    Add files via upload

---
+++ ABX-Actions/Tag-vSphere-VM-With-Tags-Object-aef2fe25c0b9948a35fbff7de1029f7b8b546babddc9b51aff4cdc23.js
@@ -0,0 +1,28 @@
+exports.handler = function handler(context, inputs) {
+  let tagsAsString = inputs.customProperties.yamlTags;
+  let tagsJsonObject = JSON.parse(tagsAsString);
+  let outputs = {};
+  outputs.tags = inputs.tags;
+  
+  console.log("Entering ABX action");
+  
+  console.log("==========================================");
+  console.log("inputs.tags:");
+  console.log(inputs.tags);
+  console.log("==========================================");
+  console.log("tagsAsString:");
+  console.log(tagsAsString);
+  console.log("==========================================");
+  console.log("tagsJsonObject:");
+  console.log(tagsJsonObject);
+  console.log("==========================================");
+ 
+  
+  Object.keys(tagsJsonObject).forEach(key => {
+      outputs.tags[key] = tagsJsonObject[key];
+      console.log("key: " + key + "  /  value: " + tagsJsonObject[key]);
+  }); 
+ 
+
+  return outputs;
+};
